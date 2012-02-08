93c5011a94f62b14c6f929f43659a85cf8b6755d
Cobus Bernard <cobus.bernard@gmail.com>
2012-02-08 15:49:02 +0200

    basic project for eclipse

---
+++ src/za/co/cobusbernard/projecteuler/Problems.java
@@ -0,0 +1,21 @@
+package za.co.cobusbernard.projecteuler;
+
+public class Problems {
+
+	/**
+	 * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+	 * The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+	 * 
+	 * @param topLimit the upper limit to calculate to.
+	 * 
+	 * @return the sum according to the question.
+	 */
+	public static int problem1(int topLimit){
+		int total = 0;
+		
+		
+		
+		return total;
+	}
+	
+}
---
+++ src/za/co/cobusbernard/projecteuler/ProblemsTest.java
@@ -0,0 +1,15 @@
+package za.co.cobusbernard.projecteuler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ProblemsTest {
+
+	@Test
+	public void test() {
+		Assert.assertEquals(23, Problems.problem1(10));
+	}
+
+}
